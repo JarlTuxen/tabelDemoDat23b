bcc5dcb21ec50b5a1614e3956a4187b0745133f1
jarltuxen <jarl.tuxen@gmail.com>
2024-03-06 09:24:00 +0100

    Initial commit

---
+++ src/main/java/dk/kea/tabeldemodat23b/TabelDemoDat23bApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.tabeldemodat23b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabelDemoDat23bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabelDemoDat23bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/tabeldemodat23b/TabelDemoDat23bApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.tabeldemodat23b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelDemoDat23bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
